d0569327974e64ecdfdf2e33cfe2e81e7736d08a
rongwenzhao <rong_wz@126.com>
2017-11-29 20:42:28 +0800

    修改双击退出失灵的bug。

--- latte-core/src/main/java/com/nicro/latte/delegates/bottom/BottomItemDelegate.java
+++ latte-core/src/main/java/com/nicro/latte/delegates/bottom/BottomItemDelegate.java
@@ -1,10 +1,9 @@
 package com.nicro.latte.delegates.bottom;
 
-import android.view.KeyEvent;
-import android.view.View;
 import android.widget.Toast;
 
 import com.nicro.latte.R;
+import com.nicro.latte.app.Latte;
 import com.nicro.latte.delegates.LatteDelegate;
 
 /**
@@ -12,44 +11,19 @@ import com.nicro.latte.delegates.LatteDelegate;
  * Created by rongwenzhao on 2017/11/25.
  */
 
-public abstract class BottomItemDelegate extends LatteDelegate implements View.OnKeyListener {
-
-    private long mExitTime = 0;
-    private static final int EXIT_TIME = 2000;
-
-    /**
-     * Fregment每次重新显示的时候，需要对其TouchMode需要重新处理，让其获得焦点。
-     * 这样，它的onKeyListener才会起作用，双击back键退出应用才会有效。
-     */
-    @Override
-    public void onResume() {
-        super.onResume();
-        final View rootView = getView();
-        if (rootView != null) {
-            rootView.setFocusableInTouchMode(true);
-            rootView.requestFocus();
-            rootView.setOnKeyListener(this);
-        }
-    }
+public abstract class BottomItemDelegate extends LatteDelegate {
+    // 再点一次退出程序时间设置
+    private static final long WAIT_TIME = 2000L;
+    private long TOUCH_TIME = 0;
 
     @Override
-    public boolean onKey(View v, int keyCode, KeyEvent event) {
-        //双重保险验证key
-        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
-            //时间间隔大于两秒
-            if (System.currentTimeMillis() - mExitTime > EXIT_TIME) {
-                Toast.makeText(getContext(), "双击退出" + getString(R.string.app_name), Toast.LENGTH_SHORT).show();
-                mExitTime = System.currentTimeMillis();
-                return true;
-            } else {//否则退出应用。下面退出的操作，根据项目具体重写。
-                _mActivity.finish();
-                if (mExitTime != 0) {
-                    mExitTime = 0;
-                }
-                //返回true，说明本事件已经被消费，不需要系统额外处理。
-                return true;
-            }
+    public boolean onBackPressedSupport() {
+        if (System.currentTimeMillis() - TOUCH_TIME < WAIT_TIME) {
+            _mActivity.finish();
+        } else {
+            TOUCH_TIME = System.currentTimeMillis();
+            Toast.makeText(_mActivity, "双击退出" + Latte.getApplicationContext().getString(R.string.app_name), Toast.LENGTH_SHORT).show();
         }
-        return false;
+        return true;
     }
 }
